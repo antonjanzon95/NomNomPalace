eab20946f42f33a7dbc1161ed0b7949b99ac7544
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-20 20:26:04 +0100

    Changed axios to fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import DataFetcher from "./components/DataFetcher";
 
 function App() {
   return (
     <>
       <Header />
+      <DataFetcher />
       <Hero />
     </>
   );
