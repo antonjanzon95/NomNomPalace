c09a7e2637bcbe424aec6adf6054c73940ffb27f
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-20 20:26:04 +0100

    Changed axios to fetch

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -1,60 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios, { AxiosResponse } from "axios";
 
-type Recipe = {
-  title: string;
-  image: string;
-  readyInMinutes: number;
-  servings: number;
-  sourceUrl: string;
-};
-
-type ResponseData = {
-  results: Recipe[];
-};
-
-interface RecipeSearchProps {
-  parameters: {
-    query: string;
-    cuisine: string;
-  };
-}
-
-const DataFetcher: React.FC<RecipeSearchProps> = ({ parameters }) => {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+const DataFetcher = () => {
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response: AxiosResponse<ResponseData> = await axios.get(
-          "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
-          {
-            params: {
-              apiKey: "86f6844e77mshc0e89cf629d9db5p1e9b99jsn9d58087ff9af",
-              ...parameters,
-            },
-          }
-        );
+    fetch(
+      "https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=e1fcb73082d845f8a97b44215050d307"
+    )
+      .then((response) => response.json())
+      .then((res) => console.log(res));
+  }, []);
 
-        setRecipes(response.data.results);
-      } catch (error: any) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [parameters]);
-
-  return (
-    <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.title}>
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image} alt={recipe.title} />
-        </div>
-      ))}
-    </div>
-  );
+  return <div>Hello</div>;
 };
 
 export default DataFetcher;
