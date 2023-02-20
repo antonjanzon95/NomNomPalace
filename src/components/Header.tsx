552aec99dff31a59d9d105402456819556a2d7e2
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-20 16:32:28 +0100

    Added Link (react-router-dom)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="w-screen">
       <nav className="container">
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
     </header>
