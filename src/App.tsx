d61858fd966a75a097b76594d2ef76d26f001c40
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-20 18:24:42 +0100

    Basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Hero = () => {
-  return <main></main>;
+  return (
+    <main className="h-screen w-screen bg-amber-50">
+      <div className="container flex justify-center pt-16">
+        <div className="w-80 h-80 bg-amber-200 border-solid border-2 rounded-xl border-emerald-700"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Hero;
