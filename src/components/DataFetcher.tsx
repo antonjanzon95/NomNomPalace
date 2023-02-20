12556c0db178b696839276204d990711e7bd689c
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-20 20:45:37 +0100

    Added props to fetch component

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -1,17 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-const DataFetcher = () => {
-  const [recipes, setRecipes] = useState([]);
+interface Recipe {
+  id: number;
+  title: string;
+  image: string;
+}
+
+interface RecipeProps {
+  query: string;
+}
+
+const DataFetcher: React.FC<RecipeProps> = ({ query }) => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
 
   useEffect(() => {
     fetch(
-      "https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=e1fcb73082d845f8a97b44215050d307"
+      `https://api.spoonacular.com/food/products/search?query=${query}&apiKey=e1fcb73082d845f8a97b44215050d307`
     )
       .then((response) => response.json())
-      .then((res) => console.log(res));
+      .then((res) => setRecipes(res.products))
+      .catch((error) => console.error(error));
   }, []);
 
-  return <div>Hello</div>;
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div
+          key={recipe.id}
+          className="h-30 w-full flex gap-10 mb-10 justify-center items-center"
+        >
+          <h2>{recipe.title}</h2>
+          <img src={recipe.image} alt={recipe.title} className="h-16 w-16" />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default DataFetcher;
