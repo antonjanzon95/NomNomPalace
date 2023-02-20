f4dad48135578ddf21bb4eb00a680c91bf72072f
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-20 19:19:55 +0100

    Reduced number of parameters

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -17,21 +17,6 @@ interface RecipeSearchProps {
   parameters: {
     query: string;
     cuisine: string;
-    diet: string;
-    intolerances: string;
-    includeIngredients: string;
-    excludeIngredients: string;
-    type: string;
-    instructionsRequired: string;
-    addRecipeInformation: string;
-    sort: string;
-    sortDirection: string;
-    minCalories: string;
-    maxCalories: string;
-    offset: string;
-    number: string;
-    limitLicense: string;
-    ranking: string;
   };
 }
 
@@ -59,45 +44,6 @@ const DataFetcher: React.FC<RecipeSearchProps> = ({ parameters }) => {
 
     fetchData();
   }, [parameters]);
-
-  // const axios = require("axios");
-
-  // const options = {
-  //   method: "GET",
-  //   url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch",
-  //   params: {
-  //     query: "pasta",
-  //     cuisine: "italian",
-  //     diet: "vegetarian",
-  //     intolerances: "gluten",
-  //     includeIngredients: "tomato,cheese",
-  //     excludeIngredients: "eggs",
-  //     type: "main course",
-  //     instructionsRequired: "true",
-  //     addRecipeInformation: "false",
-  //     sort: "calories",
-  //     sortDirection: "asc",
-  //     minCalories: "50",
-  //     maxCalories: "800",
-  //     offset: "0",
-  //     number: "10",
-  //     limitLicense: "false",
-  //     ranking: "2",
-  //   },
-  //   headers: {
-  //     "X-RapidAPI-Key": "86f6844e77mshc0e89cf629d9db5p1e9b99jsn9d58087ff9af",
-  //     "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-  //   },
-  // };
-
-  // axios
-  //   .request(options)
-  //   .then(function (response: AxiosResponse) {
-  //     console.log(response.data);
-  //   })
-  //   .catch(function (error: AxiosError) {
-  //     console.error(error);
-  //   });
 
   return <div>DataFetcher</div>;
 };
