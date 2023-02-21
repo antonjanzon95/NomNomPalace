03d07449f1961b23bb783d52cacec859de8f362f
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-21 13:24:38 +0100

    Added useState

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchForm = () => {
-  return <div>SearchForm</div>;
+const SearchForm: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <div>
+      <form action="submit">
+        <label htmlFor="query">Search for a recipe:</label>
+        <input type="text" id="query" name="query" value={searchQuery} />
+      </form>
+    </div>
+  );
 };
 
 export default SearchForm;
