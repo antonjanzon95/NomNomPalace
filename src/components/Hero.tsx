93e63c3fcbf1d3451cc0741fb9c1c7a31335edea
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-21 13:42:13 +0100

    Added useState

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import DataFetcher from "./DataFetcher";
 import SearchForm from "./SearchForm";
 
 const Hero = () => {
+  const [query, setQuery] = useState("");
+
   return (
     <main className="h-screen w-screen bg-amber-50">
       <div className="container flex flex-col items-center pt-10 gap-6">
