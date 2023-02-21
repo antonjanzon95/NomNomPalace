def25045dbf6b7feb94e1fd19dbb21582a1b2d2b
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-21 13:32:51 +0100

    Basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import DataFetcher from "./components/DataFetcher";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -14,16 +14,25 @@ const SearchForm: React.FC = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="h-36 bg-amber-200 border-solid border-2 border-emerald-700 flex flex-col justify-center items-center gap-3"
+      >
         <label htmlFor="query">Search for a recipe:</label>
         <input
+          className="border-2 rounded border-black"
           type="text"
           id="query"
           name="query"
           value={searchQuery}
           onChange={handleChange}
         />
-        <button type="submit">Search</button>
+        <button
+          type="submit"
+          className="border-solid border-black border-2 px-2 bg-gradient-to-r from-emerald-200 to-emerald-500"
+        >
+          Search
+        </button>
       </form>
     </div>
   );
