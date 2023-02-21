c5fccbefc7cc1530c5b1c9e470d8c39f40b51b37
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-21 13:26:13 +0100

    Added handleChange func

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -3,11 +3,21 @@ import React, { useState } from "react";
 const SearchForm: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div>
       <form action="submit">
         <label htmlFor="query">Search for a recipe:</label>
-        <input type="text" id="query" name="query" value={searchQuery} />
+        <input
+          type="text"
+          id="query"
+          name="query"
+          value={searchQuery}
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
