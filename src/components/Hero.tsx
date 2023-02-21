9284f1f2f894b02f094bab95dca480824138bec9
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-21 13:33:49 +0100

    Added component to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import DataFetcher from "./DataFetcher";
+import SearchForm from "./SearchForm";
 
 const Hero = () => {
   return (
     <main className="h-screen w-screen bg-amber-50">
       <div className="container flex flex-col items-center pt-10 gap-6">
         <h1 className="text-2xl">Nom Nom Palace</h1>
-        <div className="w-80 h-36 bg-amber-200 border-solid border-2 border-emerald-700 flex flex-col justify-center items-center gap-3">
-          <div>
-            <p className="text-center">Search for a recipe:</p>
-            <input type="text" className="border-2 rounded border-black" />
-          </div>
-          <div>
-            <button className="border-solid border-black border-2 px-2 bg-gradient-to-r from-emerald-200 to-emerald-500">
-              Search
-            </button>
-          </div>
-        </div>
+        <SearchForm />
       </div>
       <DataFetcher query={"cheese"} />
     </main>
